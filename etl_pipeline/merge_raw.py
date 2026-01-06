04dac5de5e316d21ae145ad7643d5699e3f4da2d
meyudha <meyudha132@gmail.com>
2026-01-06 10:52:47 +0700

    Update merge_raw.py

--- etl_pipeline/merge_raw.py
+++ etl_pipeline/merge_raw.py
@@ -1,1 +1,19 @@
+from etl_pipeline.extract_source1 import extract_source1
+from etl_pipeline.extract_source2 import extract_source2
+
+# Extract per sumber (RAW)
+df_csv_raw = extract_source1()
+df_hf_raw = extract_source2()
+
+# GABUNG RAW DATA (BELUM CLEANING)
+df = pd.concat(
+    [df_csv_raw, df_hf_raw],
+    ignore_index=True
+)
+
+print("=== DATA GABUNGAN (AWAL TRANSFORM) ===")
+print(f"Total baris : {df.shape[0]:,}")
+print(f"Total kolom : {df.shape[1]}")
+
+df.head()
 
