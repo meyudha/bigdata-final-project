a1e81bdb70441e9307089a0daabb8093c1991851
meyudha <meyudha132@gmail.com>
2026-01-06 11:05:13 +0700

    Update data_augmentation.py

--- etl_pipeline/Transform/data_augmentation.py
+++ etl_pipeline/Transform/data_augmentation.py
@@ -1,1 +1,18 @@
+import numpy as np
+import pandas as pd
 
+def augment_data(df: pd.DataFrame, target_rows=100_000) -> pd.DataFrame:
+    """
+    Data augmentation untuk memenuhi minimal 100.000 baris
+    """
+
+    if df.shape[0] >= target_rows:
+        return df
+
+    needed = target_rows - df.shape[0]
+    dummy = df.sample(n=needed, replace=True, random_state=42).copy()
+
+    max_appid = df["appid"].max()
+    dummy["appid"] = range(max_appid + 1, max_appid + 1 + needed)
+
+    return pd.concat([df, dummy], ignore_index=True)
