73269565b15bb9ed1e5363040ac0a6ac1f58ac27
meyudha <meyudha132@gmail.com>
2026-01-06 11:15:29 +0700

    Update aggregation.sql

--- elt_pipeline/aggregation.sql
+++ elt_pipeline/aggregation.sql
@@ -1,1 +1,45 @@
+-- ============================================
+-- ELT FEATURE ENGINEERING & ANALYTICS
+-- ============================================
+
+DROP TABLE IF EXISTS elt_analytics_games;
+
+CREATE TABLE elt_analytics_games AS
+SELECT
+    appid,
+    name,
+    price,
+    positive,
+    negative,
+    recommendations,
+    average_playtime_forever,
+    genres,
+    developers,
+    release_date,
+    metacritic_score,
+    user_score,
+
+    -- Feature Engineering
+    (positive + negative) AS total_reviews,
+
+    CASE
+        WHEN (positive + negative) > 0
+        THEN ROUND(positive * 1.0 / (positive + negative), 4)
+        ELSE 0
+    END AS positive_ratio,
+
+    CASE
+        WHEN price = 0 THEN 1 ELSE 0
+    END AS is_free,
+
+    CAST(strftime('%Y', release_date) AS INTEGER) AS release_year,
+
+    CASE
+        WHEN release_date IS NOT NULL
+        THEN CAST(strftime('%Y', 'now') AS INTEGER)
+             - CAST(strftime('%Y', release_date) AS INTEGER)
+        ELSE 0
+    END AS game_age_years
+
+FROM elt_clean_games;
 
