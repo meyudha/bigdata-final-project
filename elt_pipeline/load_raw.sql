9a6618f35f335dc4854370fd4b62dbe8d05d92f3
meyudha <meyudha132@gmail.com>
2026-01-06 11:14:16 +0700

    Update load_raw.sql

--- elt_pipeline/load_raw.sql
+++ elt_pipeline/load_raw.sql
@@ -1,1 +1,16 @@
+-- ============================================
+-- LOAD RAW DATA (ELT)
+-- Data mentah langsung masuk warehouse
+-- ============================================
+
+DROP TABLE IF EXISTS raw_kaggle_steam_games;
+DROP TABLE IF EXISTS raw_huggingface_steam_games;
+
+CREATE TABLE raw_kaggle_steam_games AS
+SELECT *
+FROM read_csv_auto('raw/etl/kaggle/games.csv');
+
+CREATE TABLE raw_huggingface_steam_games AS
+SELECT *
+FROM read_parquet('raw/api/huggingface/steam_games_raw.parquet');
 
